42522ba5ab0ae9d7d48760f54c5df620bfabe77a
peng <531299585@qq.com>
2017-11-06 13:58:22 +0800

    添加了验证码验证

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>@yield('title', 'LaraBBS') - post系统</title>
+    <title>@yield('title', 'peng') - post系统</title>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
