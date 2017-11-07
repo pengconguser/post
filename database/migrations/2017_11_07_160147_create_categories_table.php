c99957cd2cb2687a50e40c396448261a5ef79e6a
peng <531299585@qq.com>
2017-11-07 16:11:18 +0800

    初步创建帖子分类的模型

---
+++ database/migrations/2017_11_07_160147_create_categories_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoriesTable extends Migration {
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up() {
+		Schema::create('categories', function (Blueprint $table) {
+			$table->increments('id');
+			$table->string('name')->index()->comment('名称');
+			$table->text('description')->nullable()->comment('描述');
+			$table->integer('post_count')->default(0)->comment('帖子数');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down() {
+		Schema::dropIfExists('categories');
+	}
+}
